509ba53e1c021e09a363cfc2060d6a6f303359dd
cloeb <cloeb@purdue.edu>
2025-02-15 22:04:10 -0500

    changed install file scripts. Moved demo in.

---
+++ demo/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+package_name = 'demo'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cloeb',
+    maintainer_email='cloeb@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'talker = demo.publisher_member_function:main',
+            'listener = demo.subscriber_member_function:main',
+        ],
+    },
+)
